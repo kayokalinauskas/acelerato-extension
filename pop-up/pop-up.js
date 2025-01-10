ce6a2c4343d3f9439bef2baeb1a7767b8af10363
kayokalinauskas <kalinauskas@live.com>
2025-01-10 18:05:54 -0300

    update ticket URL in pop-up script

--- pop-up/pop-up.js
+++ pop-up/pop-up.js
@@ -2,7 +2,7 @@ const ticketInput = document.getElementById("ticketInput");
 ticketInput.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     const ticketNumberInput = ticketInput.value;
-    const url = `https://bluesoft.acelerato.com/tickets/${ticketNumberInput}`;
+    const url = `https://suporte.bluesoft.com.br/tickets/${ticketNumberInput}`;
     chrome.tabs.create({ url });
   }
 });
